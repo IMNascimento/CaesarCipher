dc3d86407f0e4f144aa2269f1e8a2c62138f4bde
Professor Igor <trabalho.computador.sjn@gmail.com>
2024-04-22 13:16:50 -0300

    atualização da imagem

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from cipher import CaesarCipher
 
 
 cifra = CaesarCipher()
-cifra.crypting('IGOR')
-cifra.descrypting('JHPS')
+#cifra.crypting('IGOR')
+#cifra.descrypting('JHPS')
+frase = input("digite sua mensagem:")
+cifra.crypting(frase)
+cifra.descrypting(cifra.crypt)
 print(cifra.crypt)
-print(cifra.descrypt)
+#print(cifra.descrypt)
